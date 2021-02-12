dbbb9173a551ff2237e0f68686b29dac48b34334
filemakr <filemakr@gmail.com>
2021-02-12 12:03:20 +0530

    a

--- routes/pool.js
+++ routes/pool.js
@@ -3,10 +3,10 @@ require('dotenv').config()
 
 const pool = mysql.createPool({
 
-   host:'idlcdatabase-do-user-8079481-0.b.db.ondigitalocean.com',
+   host:'db-mysql-blr1-35944-do-user-8703439-0.b.db.ondigitalocean.com',
   ///host : 'localhost',
    user: 'doadmin',
-  password:'zrfsltih9so8u9zk',
+  password:'r5c1d14p7xg6e2sr',
   // password : '123',
     database: 'political_frames',
     port:'25060' ,
